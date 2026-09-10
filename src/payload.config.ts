f37bfb1ed65633b64a2c7252b65e7c319fef8a3c
EagleFox31 <86088743+EagleFox31@users.noreply.github.com>
2026-09-10 19:31:04 +0100

    chore: remove one-time editorial import action

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -30,7 +30,6 @@ export default buildConfig({
       // The `BeforeLogin` component renders a message that you see while logging into your admin panel.
       // Feel free to delete this at any time. Simply remove the line below.
       beforeLogin: ['@/components/BeforeLogin'],
-      beforeDashboard: ['@/components/BeforeDashboard'],
     },
     importMap: {
       baseDir: path.resolve(dirname),
